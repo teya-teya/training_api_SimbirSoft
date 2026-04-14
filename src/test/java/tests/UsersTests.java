b1e5f4e4b3b1fb8b45a72f7afaddfeda393fa611
teya-teya <teya.teya89@yandex.ru>
2026-04-14 21:34:50 +0300

    QA-001 убраны неиспользуемые импорты

--- src/test/java/tests/UsersTests.java
+++ src/test/java/tests/UsersTests.java
@@ -11,10 +11,8 @@ import io.qameta.allure.Story;
 import io.restassured.response.Response;
 import models.UserRequest;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import javax.script.ScriptEngine;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
